1e652e82a910e45a901b5720fb458f02b5573491
Ondrej <svoren258@gmail.com>
2016-09-29 22:08:48 +0200

    Skuska

---
+++ main.c
@@ -0,0 +1,14 @@
+/********************************************************************/
+/*Projekt:Implementace interpretu imperativního jazyka IFJ14        */
+/*Jména řešitelů: Sebastián Kisela, Ondrej Svoreň, Daniel Rudík,    */
+/*                  Patrik Roman, Martin Chudý                      */
+/*Loginy řešitelů: xsvore01, xkisel02, xrudik00                     */
+/********************************************************************/
+
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    printf("Hello world\n");
+    return 0;
+}
