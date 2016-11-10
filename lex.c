4dbdadb84d7170787bfedc2b72a6dc1fe0439fbf
Martin Chudý <xchudy04@stud.fit.vutbr.cz>
2016-11-10 22:05:35 +0100

    Added comments to TState, token structure changed

--- lex.c
+++ lex.c
@@ -1,10 +1,12 @@
 #include "lex.h"
+#include "main.h"
 
 
 Ttoken *token;
 
 char *keywords[] ={"boolean","break","class","continue","do","double","else","false","for","if","int","return",
 	"String","static","true","void","while"};
+
 
 
 void lex_init(){
