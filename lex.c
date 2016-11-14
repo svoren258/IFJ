4258e37af93f654a11716cfb58a0c068b62edc89
Roman Patrik <xroman10@merlin.fit.vutbr.cz>
2016-11-14 15:26:04 +0100

    new version of lexical analysis

--- lex.c
+++ lex.c
@@ -1,5 +1,6 @@
 #include "lex.h"
 #include <string.h>
+#include <ctype.h>
 
 #define COUNT_OF_KEYWORDS 17
 
@@ -52,7 +53,7 @@ Ttoken *get_token(Ttoken *token)
 
 	token->data = malloc(20);
 
-	while(cont && (c = getc(file)))
+	while(!cont && (c = getc(file)))
 	{
 		switch(type)
 		{
@@ -113,6 +114,61 @@ Ttoken *get_token(Ttoken *token)
 				else if(c == ';')
 				{
 					type = COLON;
+					break;
+				}
+				else if(c == EOF)
+				{
+					type = END;
+					break;
+				}
+				else if(c == '<')
+				{
+					type = LESSER;
+					break;
+				}
+				else if(c == '>')
+				{
+					type = GREATER;
+					break;
+				}
+				else if(c == '!')
+				{
+					type = EXPL;
+					break;
+				}
+				else if(c == '=')
+				{
+					type = EQUATE;
+					break;
+				}
+				else if(c == '&')
+				{
+					type = AND_A;
+					break;
+				}
+				else if(c == '|')
+				{
+					type = OR_A;
+					break;
+				}
+				else if(c == '+')
+				{
+					type = PLUS;
+					break;
+				}
+				else if(c == '-')
+				{
+					type = MINUS;
+					break;
+				}
+				else if(c == '*')
+				{
+					type = MUL;
+					break;
+				}
+				else if(c == '/')
+				{
+					type = DIV;
 					break;
 				}
 
@@ -245,6 +301,7 @@ Ttoken *get_token(Ttoken *token)
 
             	break;
 			}
+
 			case N_DEC:
 			{
 				if(isdigit(c))
@@ -262,16 +319,18 @@ Ttoken *get_token(Ttoken *token)
 					type = N_DEC_E;
 					ExtendToken(&token, lenght);
 				}
-				else //dodělat
+				else 
 				{
-					token->type=INTGR;
+					token->type = INTGR;
 					type = END;
                 	if (!isspace(c))
                 	{
         				ungetc(c, file);
                 	}
 				}
+				break;
 			}
+
 			case N_DEC_E:
 			{
 				if(isdigit(c))
@@ -279,16 +338,18 @@ Ttoken *get_token(Ttoken *token)
 					type N_DEC_E;
 					ExtendToken(&token, lenght);
 				}
-				else //dodělat
+				else 
 				{
-					token->type=INTGR;
+					token->type = INTGR;
 					type = END;
                 	if (!isspace(c))
                 	{
         				ungetc(c, file);
                 	}
 				}
+				break;
 			}
+
 			case DEC:
 			{
 				if(isdigit(c))
@@ -301,16 +362,18 @@ Ttoken *get_token(Ttoken *token)
 					type DEC_E;
 					ExtendToken(&token, lenght);
 				}
-				else //dodělat
+				else 
 				{
-					token->type=DBLE;
+					token->type = DBLE;
 					type = END;
                 	if (!isspace(c))
                 	{
         				ungetc(c, file);
                 	}
 				}
+				break;
 			}
+
 			case DEC_E:
 			{
 				if(isdigit(c))
@@ -318,16 +381,131 @@ Ttoken *get_token(Ttoken *token)
 					type DEC_E;
 					ExtendToken(&token, lenght);
 				}
-				else //dodělat
+				else 
 				{
-					token->type=DBLE;
+					token->type = DBLE;
 					type = END;
                 	if (!isspace(c))
                 	{
         				ungetc(c, file);
                 	}
 				}
+				break;
+			}
+
+			case MUL:
+			{
+				if(c == '/')
+				{
+					token->type = B_COM_B;
+					ExtendToken(&token, lenght);
+				}
+				else
+				{
+					token->type = type;
+                	type = END;
+                	if (!isspace(c))
+                	{
+        				ungetc(c, file);
+                	}
+				}
+				break;
+			}
+
+			case DIV:
+			{
+				if(c == '/')
+				{
+					token->type = COM;
+					ExtendToken(&token, lenght);
+				}
+				else if(c == '*')
+				{
+					token->type = B_COM_A;
+					ExtendToken(&token, lenght);
+				}
+				else
+				{
+					token->type = type;
+                	type = END;
+                	if (!isspace(c))
+                	{
+        				ungetc(c, file);
+                	}
+				}
+				break;
+			}
+
+			case ID:
+			{
+				if ((isalpha(c) || isdigit(c) || (c == '_')))
+				{
+					token->type = type;
+					ExtendToken(&token, lenght);
+				}
+				else
+				{
+					token->type = IsKeyword(&token)
+					type = END;
+					if (!isspace(c))
+                	{
+        				ungetc(c, file);
+                	}
+				}
+				break;
+			}
+
+			case DOT:
+			case COMMA:
+			case LB:
+			case PB:
+			case LSB:
+			case LPB:
+			case LDB:
+			case PDB:
+			case MOD:
+			case AND_B:
+			case OR_B:
+			case SEM_CL:
+			case QUOTE:
+			case COLON:
+			case EQUALS:
+			case N_EQ:
+			case LE_EQ:
+			case GR_EQ:
+			case MUL:
+			case DIV:
+			case COM:
+			case B_COM_A:
+			case B_COM_B:
+			case KEYWORDS:
+
+			{
+				token->type = type;
+				type = END;
+				if (!isspace(c))
+                	{
+        				ungetc(c, file);
+                	}
+                break;
+			}
+
+			case END:
+			{
+				if (!isspace(c))
+                	{
+        				ungetc(c, file);
+                	}
+                cont = true;
+                break;
 			}
 		}
-	}	
+
+		/*if(error)
+		{
+			break;
+		}*/
+	}
+	return *token; //to záleží	
 }
+
