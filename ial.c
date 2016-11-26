1c7aec37681680c058233ca50f87f9dbf2234d51
xrudik00 <daniel.rudik@gmail.com>
2016-11-26 13:33:20 +0100

    Update ial.c

--- ial.c
+++ ial.c
@@ -13,6 +13,104 @@
 // 		return new_node;
 // }
 
+int find(TVariable* s, TVariable* search){
+    char* str = s1->value.c;
+    char* search_str = search1->value.c;
+
+    int size_s = length(str);
+    int size_search = length(search_str);
+    if(size_search == 0)
+        return 0;
+    bool match = FALSE;
+    int* prefix_t = malloc((size_search+1)* sizeof(int));
+
+    prefix_t[0] = -1;
+    for (int k = 0; k < size_search; ++k) {
+        prefix_t[k+1] = prefix_t[k] + 1;
+        while(prefix_t[k+1] > 0 && (search_str[k] != search_str[prefix_t[k+1]-1])) {
+            prefix_t[k+1] = prefix_t[prefix_t[k+1]-1]+1;
+        }
+    }
+
+    for (int i = 0; i < size_s; ++i) {
+        for (int j = 0; j < size_search; ++j) {
+            if(str[i+j] == search_str[j])
+                match = TRUE;
+            else{
+                match = FALSE;
+                break;
+            }
+        }
+        if(match == TRUE) {
+            free(prefix_t);
+            return i;
+        }
+    }
+    free(prefix_t);
+    return -1;
+}
+
+char* sort(TVariable* s){
+    char* str = s->value.c;
+
+    int lngth = length(str);
+    sorting(str,lngth);
+
+    return str;
+}
+
+void sorting(char* s,int length){
+
+    if(length <= 1)
+        return;
+
+
+    int mid = length/2;
+    char* left = (char*) malloc(mid);
+    for (int i = 0; i < mid; ++i) {
+        left[i] = s[i];
+    }
+
+    char* right = (char*) malloc(length-mid);
+    for (int j = mid; j < length; ++j) {
+        right[j-mid] = s[j];
+    }
+
+    sorting(left, mid);
+    sorting(right, length-mid);
+    merge(s, left, mid, right, length-mid);
+    free(left);
+    free(right);
+
+}
+void merge(char* s,char* left,int l_length,char* right,int r_length){
+    int i = 0;
+    int j = 0;
+
+    while((i < l_length) && (j < r_length)){
+        if(left[i] < right[j]){
+            s[i+j] = left[i];
+            i++;
+        }
+        else{
+            s[i+j] = right[j];
+            j++;
+        }
+    }
+    if (i < l_length){
+        while (i < l_length) {
+            s[i + j] = left[i];
+            i++;
+        }
+    }
+    else {
+        while (j < r_length) {
+            s[i + j] = right[j];
+            j++;
+        }
+    }
+}
+
 void BSTInit (tTablePtr *RootPtr) {
 
 	(*RootPtr) = NULL;	
