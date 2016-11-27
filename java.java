a67de9505b3f64bb4a517abd98f055100f0bd985
Sebastian Kisela <sebastian.kisela@gmail.com>
2016-11-27 16:34:43 +0000

    push not compelte interpet

--- java.java
+++ java.java
@@ -7,21 +7,19 @@ class Main
         str1 = "Toto je text";
         
         ifj16.print("Pozice retezce \"text\" v retezci str2: " + str1 + 4+ "\n");
-        // Game.hello(str1);
+        Game.hello(str1);
         
+        ifj16.print(str1);
         
     } // end of static void run()
     
     
 } // end of class Main
 
-// class Game
-// {
-//     static int c = 4;
-//     static void hello(String str1)
-//     {
-//         int var = 4;
-//         int a = 5;
-//         ifj16.print("hello " + str1 + "\n");
-//     }
-// }
+class Game
+{
+    static void hello(String str1)
+    {
+        str1 = "ZMeneny text";
+    }
+}
